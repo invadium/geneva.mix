5f46a0b2173c2dac6f2865d4e5fbc27354cda4fd
Igor Khotin <khotin@gmx.com>
2025-05-28 02:15:19 +0300

    [*] pause box autoprogression on Left-Right cursor keys and resume on Space

--- mod/trap/keyDown.js
+++ mod/trap/keyDown.js
@@ -3,12 +3,18 @@ function keyDown(e) {
 
     switch(e.code) {
         case 'Space':
+            lab.boxControl.nextBox()
+            lab.boxControl.paused = false
+            break
+
         case 'ArrowRight':
             lab.boxControl.nextBox()
+            lab.boxControl.paused = true
             break
 
         case 'ArrowLeft':
             lab.boxControl.prevBox()
+            lab.boxControl.paused = true
             break
 
         case 'KeyP':
